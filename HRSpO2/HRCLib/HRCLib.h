de027c728417f493c284e7136308a12d26ffb7e2
lghchinaidea <lghchinaidea@gmail.com>
2019-01-17 16:48:26 +0800

    cli lib

---
+++ HRSpO2/HRCLib/HRCLib.h
@@ -0,0 +1,14 @@
+// HRCLib.h
+
+#pragma once
+
+using namespace System;
+
+namespace HRCLib {
+
+	public ref class HRCLib
+	{
+		// TODO: Add your methods for this class here.
+
+	};
+}
---
+++ HRSpO2/HRCLib/max30102_algo.h
@@ -0,0 +1,33 @@
+#ifndef MAX30102_ALGO_H
+#define MAX30102_ALGO_H
+
+//#include "ble.h"
+#include "stdbool.h"
+#include "stdint.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define FS 100
+#define BUFFER_SIZE  (FS* 5)
+#define HR_FIFO_SIZE 7
+#define MA4_SIZE  4 // DO NOT CHANGE
+#define HAMMING_SIZE  5// DO NOT CHANGE
+#define min(x,y) ((x) < (y) ? (x) : (y))
+
+void maxim_heart_rate_and_oxygen_saturation(uint32_t *pun_ir_buffer ,  int32_t n_ir_buffer_length, uint32_t *pun_red_buffer ,   int32_t *pn_spo2, int8_t *pch_spo2_valid ,  int32_t *pn_heart_rate , int8_t  *pch_hr_valid);
+void maxim_find_peaks( int32_t *pn_locs, int32_t *pn_npks,  int32_t *pn_x, int32_t n_size, int32_t n_min_height, int32_t n_min_distance, int32_t n_max_num );
+void maxim_peaks_above_min_height( int32_t *pn_locs, int32_t *pn_npks,  int32_t *pn_x, int32_t n_size, int32_t n_min_height );
+void maxim_remove_close_peaks( int32_t *pn_locs, int32_t *pn_npks,   int32_t  *pn_x, int32_t n_min_distance );
+void maxim_sort_ascend( int32_t *pn_x, int32_t n_size );
+void maxim_sort_indices_descend(  int32_t  *pn_x, int32_t *pn_indx, int32_t n_size);
+
+void maxim_get_an_dx(int32_t* pbuffer, int32_t buff_length);
+void maxim_get_an_dx_before_hamming(int32_t* pbuffer, int32_t buff_length);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /*MAX30102_ALGO_H*/
